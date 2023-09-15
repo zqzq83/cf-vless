62d3c29f5ccf73507b2214d841cbe89ca0c15d99
zqzq83 <56616597+zqzq83@users.noreply.github.com>
2023-09-15 15:31:34 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '8247a659-7339-5388-5066-a5dab9087055';
 
 const proxyIPs = ['cf.121112.eu.org', 'cf.sirbei.com'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
@@ -12,11 +12,11 @@ let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 let dohURL = 'https://dns.google/dns-query'; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
 // v2board api environment variables
-let nodeId = ''; // 1
+let nodeId = '1'; // 1
 
-let apiToken = ''; //abcdefghijklmnopqrstuvwxyz123456
+let apiToken = 'abcdefghijklmnopqrstuvwxyz123456'; //abcdefghijklmnopqrstuvwxyz123456
 
-let apiHost = ''; // api.v2board.com
+let apiHost = 'api.v2board.com'; // api.v2board.com
 
 if (!isValidUUID(userID)) {
     throw new Error('uuid is not valid');
