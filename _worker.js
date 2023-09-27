05d2c05250a7ab5104fe9bbab86adafce2f589d5
zqzq83 <56616597+zqzq83@users.noreply.github.com>
2023-09-27 21:23:56 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -6,6 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'f3838f3e-abe7-41b7-9c70-e2666a465e0a';
 
+
 const proxyIPs = ['103.200.112.108']; // ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
