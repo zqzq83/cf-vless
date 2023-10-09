100ff9efac3cca3712b09f36c158493e4466886a
zqzq83 <56616597+zqzq83@users.noreply.github.com>
2023-10-09 20:27:47 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -9,7 +9,8 @@ let userID = 'f3838f3e-abe7-41b7-9c70-e2666a465e0a';
 const proxyIPs = ['104.28.156.227']; // ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
-let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
+let dohURL = 'https://dns.google/dns-query
+'; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
 // v2board api environment variables
 let nodeId = ''; // 1
